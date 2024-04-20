a64e56275e0712de17dcf72d73c687e041840484
moadhamousti <118165767+moadhamousti@users.noreply.github.com>
2024-04-20 22:31:09 +0100

    UPDATE

--- src/app/admin/dashboard/posts/page.jsx
+++ src/app/admin/dashboard/posts/page.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 import styles from "./posts.module.css"
 import { fetchPosts } from '@/lib/data'
 import { format } from 'date-fns';
-import WhatsApp from "../../../../../public/phone.png"
+import WhatsApp from "../../../../../public/whatsapp.svg"
 import Phone from "../../../../../public/phone.png"
 import { Badge } from '@/components/ui/badge';
 import { deletePost } from '@/lib/actions'
--- src/components/form/ProfileForm.jsx
+++ src/components/form/ProfileForm.jsx
@@ -132,6 +132,8 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/
 import Loader from '../Loader';
 import loader from '../../../public/loader.gif'
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
 
 
 
@@ -141,7 +143,14 @@ const ProfileForm = () => {
   const [newUserName, setNewUserName] = useState(session?.user?.username || '');
   const [newEmail, setNewEmail] = useState(session?.user?.email || '');
   const [newPassword, setNewPassword] = useState(session?.user?.password || '');
-  
+  const router = useRouter();
+
+
+  useEffect(() => {
+    if (!session || status !== 'authenticated') {
+      router.push('/sign-in');
+    }
+  }, [session, status, router]);
 
   // const [imagePreview, setImagePreview] = useState("");
   const [file, setFile] = useState(null);
