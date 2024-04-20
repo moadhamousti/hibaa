1cb6fc4340574def9ea0cbe95fbae374a55e8d35
moadhamousti <118165767+moadhamousti@users.noreply.github.com>
2024-04-20 11:46:21 +0100

    Location API UPDATE

--- src/app/api/posts/filter/location/route.js
+++ src/app/api/posts/filter/location/route.js
@@ -1,21 +1,21 @@
-// import { db } from "@/lib/db";
-// import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
-// export const GET = async (req) => {
-//     try {
-//         const { searchParams } = new URL(req.url);
-//         const loc = searchParams.get("loc"); // Change to loc instead of category
+export const GET = async (req) => {
+    try {
+        const { searchParams } = new URL(req.url);
+        const loc = searchParams.get("loc"); // Change to loc instead of category
 
-//         const DonPost = await db.DonPost.findMany({
-//             include: { user: true },
-//             where: {
-//                 ...(loc && { location: loc }), // Change category to location   
-//             }
-//         });
+        const DonPost = await db.DonPost.findMany({
+            include: { user: true },
+            where: {
+                ...(loc && { location: loc }), // Change category to location   
+            }
+        });
 
-//         return new NextResponse(JSON.stringify(DonPost), { status: 200 });
-//     } catch (err) {
-//         console.error(err);
-//         return new NextResponse(JSON.stringify({ message: 'Something went wrong' }), { status: 500 });
-//     }
-// };
+        return new NextResponse(JSON.stringify(DonPost), { status: 200 });
+    } catch (err) {
+        console.error(err);
+        return new NextResponse(JSON.stringify({ message: 'Something went wrong' }), { status: 500 });
+    }
+};
