87c3dd4737135ee093ffd970551afcfc2e92c911
moadhamousti <118165767+moadhamousti@users.noreply.github.com>
2024-04-21 11:03:49 +0100

    UPDATE!

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,30 @@
+"use client"
 import Navbar from '@/components/Navbar';
 import '../app/globals.css';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
 import Provider from '@/components/Provider';
-import NProgressComponent from '@/components/NProgress';
+import Footer from '@/components/Footer';
+import NProgress from 'nprogress';
+import { usePathname, useRouter } from 'next/navigation'; 
+import { useEffect } from 'react';
 
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const router = useRouter();
+  useEffect(() => {
+    NProgress.configure({ trickleRate: 0.02, trickleSpeed: 800 });
+    NProgress.start();
+    setTimeout(()=>{
+      NProgress.done();
+    }, 3000)
+  }, [pathname]);
   return (
     <html lang='en' className="!scroll-smooth">
       <body >
           <Provider>
-            <NProgressComponent>
               <main className=''>
                 <div className=''>
                   {/* <Navbar /> */}
@@ -21,7 +33,6 @@ export default function RootLayout({ children }) {
                 </div>
               </main>
               <Toaster/>
-              </NProgressComponent>
           </Provider>
       </body>
     </html>
